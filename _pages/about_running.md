653a25addcce76cc04b56128d421c4b2dbf62c90
arohatgi29 <143004166+arohatgi29@users.noreply.github.com>
2026-03-10 06:02:31 +0000

    Deploying to gh-pages from @ arohatgi29/arohatgi29.github.io@5436f0666fefe99165da9fa8af624c7456e1c748 🚀

---
+++ _pages/about_running.md
@@ -0,0 +1,5 @@
+Outside of research, I love running. There is something about the rhythm of a long run that clears my head and helps me think through problems in ways that sitting at a desk never quite does. I started running thanks to my man, who introduced me to it, and it has since become one of my favorite ways to recharge.
+
+[Photo: UT Austin running track]
+
+More to come soon on this page!
--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,12 +30,12 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
+        },{id: "nav-running",
+          title: "Running",
+          description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/people/";
+            window.location.href = "/running/";
           },
         },{id: "dropdown-bookshelf",
               title: "bookshelf",
