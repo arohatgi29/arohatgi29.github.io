d107e08753bd34479b2a462e4dd98dd10507a84d
arohatgi29 <143004166+arohatgi29@users.noreply.github.com>
2026-03-12 02:22:13 +0000

    Deploying to gh-pages from @ arohatgi29/arohatgi29.github.io@1edd8032cee79e1a007618b86d9c955c13ed0610 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -31,7 +31,7 @@ ninja.data = [{
             window.location.href = "/cv/";
           },
         },{id: "nav-running",
-          title: "Running",
+          title: "running",
           description: "",
           section: "Navigation",
           handler: () => {
